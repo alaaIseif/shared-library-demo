658ed3e484dcb28c848070fdc7df9870dd8f0d5b
Alaa Seif <alaa.seif3112@gmail.com>
2023-10-08 22:09:23 +0300

    Update installNginx.groovy

--- vars/installNginx.groovy
+++ vars/installNginx.groovy
@@ -1,8 +1,8 @@
 def call() {
   node('worker1') {
-        stage('Clone Nginx Repository') {
-            // git 'https://github.com/nginx/nginx.git'
-          git -v
+       stage('Installation') {
+            sh 'apt-get update'
+            sh 'apt-get install -y nginx'
         }
     }
 }  
